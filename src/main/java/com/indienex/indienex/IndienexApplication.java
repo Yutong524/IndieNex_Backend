44bf4e94ace48a4b78ff82da23fd6ed59f4a02bd
Yutong Xia <143774012+Yutong524@users.noreply.github.com>
2025-02-26 16:25:23 -0800

    backend setup

---
+++ src/main/java/com/indienex/indienex/IndienexApplication.java
@@ -0,0 +1,13 @@
+package com.indienex.indienex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndienexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndienexApplication.class, args);
+	}
+
+}
